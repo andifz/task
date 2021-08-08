9a7df8e13b6b414be75468b1758d1c1c827fdc49
andi faizal <andifaizal@refactory.id>
2021-08-08 10:40:35 +0700

    update folder

--- 2/one.js
+++ 2/one.js
@@ -2,35 +2,25 @@ const axios = require("axios");
 
 // let posts = 'https://jsonplaceholder.typicode.com/posts'
 // let users = 'https://jsonplaceholder.typicode.com/users'
-async function getPost() {
+async function getPosts() {
   try {
     const response = await axios.get(
       "https://jsonplaceholder.typicode.com/posts"
     );
     console.log(response);
   } catch (error) {
-    //   console.error(error);
+    console.error(error);
   }
 }
 
-async function getUser() {
+async function getUsers() {
   try {
     const res = await axios.get("https://jsonplaceholder.typicode.com/users");
     console.log(res);
   } catch (err) {
-    // console.log(err)
+    console.log(err);
   }
 }
 
-getPost();
-
-// console.log(getPost())
-// console.log(getPost())
-// Promise.all([getPost(), getUser()])
-//   .then(function (results) {
-//     const acct = results[0];
-//     const perm = results[1];
-//   });
-
-// const getPosts =axios.get(posts)
-// const getUser =axios.get(users)
+console.log(getPosts());
+console.log(getUsers());
